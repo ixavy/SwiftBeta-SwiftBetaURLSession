de64baa8f53f3ae6278774943d144e09d57432c2
iXavy <ict@ixavy.com>
2022-10-12 12:41:08 +0200

    Initial Commit

---
+++ SwiftBetaURLSession/SwiftBetaURLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBetaURLSessionApp.swift
+//  SwiftBetaURLSession
+//
+//  Created by iXavy on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBetaURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
